58038d49c4a2eea8380fefa71be8f6f173b11789
Ray C Horn <ray@securex.ai>
2021-08-26 15:22:13 -0600

    updates.

--- utils/dockergoo.py
+++ utils/dockergoo.py
@@ -20,6 +20,15 @@ __is_prod__ = __host__ in ['DESKTOP-9J3LL5Q']
 from vyperlogix.misc import _utils
 from vyperlogix.decorators import tunnel
 
+ssh_pkey = os.environ.get('PRIVKEY')
+assert _utils.is_something(ssh_pkey), 'ssh_pkey is not set.  Check your .env file.'
+
+remote_bind_address = os.environ.get('REMOTE_DOCKER')
+assert _utils.is_something(remote_bind_address), 'remote_bind_address is not set.  Check your .env file.'
+
+local_bind_port = os.environ.get('DOCKER_PORT')
+
+local_bind_address = '127.0.0.1:{}'.format(local_bind_port)
 
 def get_volumes(containers, type='volume'):
     volumes = []
@@ -33,26 +42,54 @@ def get_volumes(containers, type='volume'):
 
 if (__name__ == "__main__"):
     fname = os.path.curdir + '/report.json'
-    with open(fname, 'w') as fOut:
-        @tunnel.ssh_tunnel(remote='10.0.0.179:22', ssh_username='raychorn', ssh_pkey='~/.ssh/id_rsa_desktop-JJ95ENL_no_passphrase', remote_bind_address='127.0.0.1:2375', local_bind_address='127.0.0.1:12375')
-        def do_the_thing(**kwargs):
-            _utils.os_command('netstat -tunlp', message='(***)', verbose=True)
-            host = os.environ.get('DOCKER_HOST{}'.format('' if (__is_prod__) else '_DEV'))
-            assert host, 'DOCKER_HOST is not set.'
-            client = docker.DockerClient(base_url=host)
-            containers = []
-            volumes = get_volumes(containers)
-            all_volumes = client.volumes.list()
-            for c in client.containers.list():
-                print(c.name)
-                containers.append(c.attrs)
-                if (0):
-                    j = json.dumps(c.attrs, indent=4)
-                    if (1):
-                        print(j, file= fOut)
-                    print('='*30)
-                    print()
-            print(json.dumps(containers, indent=4), file= fOut)
 
-        do_the_thing()
+    sources = os.environ.get('SOURCE')
+    assert _utils.is_something(sources), 'SOURCE is not set.  Check your .env file.'
+    sources = eval(sources)
+    the_sources = []
+    for name in sources:
+        addr = os.environ.get(name)
+        assert _utils.is_something(addr), 'The deployment: {} is not set.  Check your .env file.'.format(name)
+        the_sources.append({'name': name, 'addr': addr})
+
+    deployments = os.environ.get('DEPLOYMENT')
+    assert _utils.is_something(deployments), 'DEPLOYMENT is not set.  Check your .env file.'
+    deployments = eval(deployments)
+
+    the_deployments = []
+    for name in deployments:
+        addr = os.environ.get(name)
+        assert _utils.is_something(addr), 'The deployment: {} is not set.  Check your .env file.'.format(name)
+        the_deployments.append({'name': name, 'addr': addr})
+
+    for a_deployment in the_deployments:
+        remote_addr = a_deployment.get('addr')
+        assert _utils.is_something(remote_addr), 'The deployment addr: {} is not set.  Check your .env file.'.format(remote_addr)
+        ssh_username = remote_addr.split('@')[0]
+        remote = remote_addr.split('@')[-1]
+        assert _utils.is_something(remote), 'The deployment IP: {} is not set.  Check your .env file.'.format(remote)
+        if (remote.fine(':') == -1):
+            remote = '{}:{}'.format(remote, 22)
+        with open(fname, 'w') as fOut:
+            @tunnel.ssh_tunnel(remote=remote, ssh_username=ssh_username, ssh_pkey=ssh_pkey, remote_bind_address=remote_bind_address, local_bind_address=local_bind_address)
+            def do_the_thing(**kwargs):
+                _utils.os_command('netstat -tunlp', message='(***)', verbose=True)
+                host = os.environ.get('DOCKER_HOST{}'.format('' if (__is_prod__) else '_DEV'))
+                assert host, 'DOCKER_HOST is not set.'
+                client = docker.DockerClient(base_url=host)
+                containers = []
+                volumes = get_volumes(containers)
+                all_volumes = client.volumes.list()
+                for c in client.containers.list():
+                    print(c.name)
+                    containers.append(c.attrs)
+                    if (0):
+                        j = json.dumps(c.attrs, indent=4)
+                        if (1):
+                            print(j, file= fOut)
+                        print('='*30)
+                        print()
+                print(json.dumps(containers, indent=4), file= fOut)
+
+            do_the_thing()
         print('Done.')
